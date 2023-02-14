63884c504882ad9f06af236275c6b81c55728478
lina <me@jackli.dev>
2023-02-14 09:10:20 -0700

    feat: finish index

--- src/handlers/index/index.go
+++ src/handlers/index/index.go
@@ -1,10 +1,22 @@
 package index
 
 import (
+	"encoding/json"
+
 	"github.com/valyala/fasthttp"
 )
 
 func IndexHandler(ctx *fasthttp.RequestCtx) {
-	ctx.SetContentType("text/html")
-	ctx.WriteString("Hello World!")
+	ctx.Response.Header.SetCanonical([]byte("Content-Type"), []byte("application/json"))
+	ctx.Response.SetStatusCode(200)
+	response := &IndexResponse{
+		Status: 200,
+		Data: &IndexData{
+			Message: "jckli api v1",
+			Link: "https://jackli.dev",
+		},
+	}
+	if err := json.NewEncoder(ctx).Encode(response); err != nil {
+		ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
+	}
 }
