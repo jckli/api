38d235935f82a28dd32a1057a59d8e495aff3a51
lina <me@jackli.dev>
2023-02-14 08:54:10 -0700

    feat: initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"github.com/fasthttp/router"
+	"github.com/valyala/fasthttp"
+	_ "github.com/joho/godotenv/autoload"
+
+	"github.com/jckli/api/src/routes"
+)
+
+func main() {
+	r := router.New()
+	routes.InitRoutes(r)
+	port := os.Getenv("PORT")
+	fmt.Println("API on port: " + port)
+	log.Fatal(fasthttp.ListenAndServe(":" + port, r.Handler))
+}
+
